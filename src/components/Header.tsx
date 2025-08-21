1a7c77d8afb33fed8a44988610d2ffebe237177f
karamazov <samuel.silveira@sp.senai.br>
2025-08-20 22:45:38 -0300

    implementação do filtro de pesquisa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,54 @@
 import { Link, useNavigate } from "react-router-dom";
 import { IoIosReturnLeft } from "react-icons/io";
 import "./Header.css";
+import { Button } from "react-bootstrap";
+import { IoIosSearch } from "react-icons/io";
+import { useEffect, useState } from "react";
+import type { Cake } from "../types/Cake";
+import axios from "axios";
 
 export const Header = () => {
   const navigate = useNavigate();
+  const [cakes, setCakes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const irPara = (categoria: string) => {
+    navigate(`/cakes/${categoria}`);
+    setSearch("");
+  }
+
+  const getData = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/cakes");
+      const dados = res.data;
+      if (res.status === 200) {
+        setCakes(dados);
+      }
+    } catch (error) {
+      console.error("Erro ao carregar dados: ", error);
+    }
+  }
+
+  const filtered = cakes.filter((cake: Cake) => {
+    const text = search.toLowerCase();
+    return (
+      cake.name.toLowerCase().includes(text) ||
+      cake.description.toLocaleLowerCase().includes(text) ||
+      cake.category.some((cat) => cat.toLowerCase().includes(text))
+    )
+  })
+
+  useEffect(() => {
+    getData();
+    if (search.trim() === "") {
+      setSuggestions([]);
+      return;
+    }
+    setSuggestions(filtered);
+  }, [search])
+
+  
 
   return (
     <nav
@@ -16,15 +61,30 @@ export const Header = () => {
           <IoIosReturnLeft size={25} color="black" onClick={() => navigate(-1)} />
         </Link>
 
-        <div id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto align-items-end">
-            <li className="nav-item">
-              <Link className="nav-link logo-titulo" to="/">
-                <span>Bolos do <b className="jacquin">Jacquin</b></span>
-              </Link>
-            </li>
-          </ul>
+        <div className="container-pesquisar position-relative">
+          <input
+            className="inputStyle"
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Buscar bolos..."
+          />
+          <Button variant="light"><IoIosSearch /></Button>
+
+          {suggestions.length > 0 && (
+            <ul className="suggestions-list">
+              {suggestions.map((cake: Cake) => (
+                <li key={cake.id}>
+                  <p onClick={() => irPara(cake.category[0])}>{cake.name}</p>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
+
+        <Link className="nav-link logo-titulo" to="/">
+          <span>Bolos do <b className="jacquin">Jacquin</b></span>
+        </Link>
 
       </div>
 
